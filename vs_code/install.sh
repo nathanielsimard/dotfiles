ba2119e80ae85312dd16ac67af3c9c428d447d54
nathaniel <nathaniel.simard.1@ulaval.ca>
2018-02-12 22:36:57 -0500

    Add vscode extensions

--- vs_code/install.sh
+++ vs_code/install.sh
@@ -4,3 +4,10 @@ echo "Installing vs code config"
 
 cp keybindings.json ~/.config/Code/User/
 cp settings.json ~/.config/Code/User/
+
+code --install-extension vscodevim.vim
+code --install-extension rust-lang.rust
+code --install-extension ms-vscode.cpptools
+code --install-extension will-stone.plastic
+
+
