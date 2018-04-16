cf1abc1131b26029c4d0276b7b8ff9eaa6e2d4a5
nathanielsimard <nathaniel.simard.1@ulaval.ca>
2018-04-16 17:57:25 -0400

    Add Java support VsCode

--- vs_code/install.sh
+++ vs_code/install.sh
@@ -5,14 +5,21 @@ echo "Installing vs code config"
 cp keybindings.json ~/.config/Code/User/
 cp settings.json ~/.config/Code/User/
 
+# General
 code --install-extension vscodevim.vim
-code --install-extension rust-lang.rust
-code --install-extension ms-vscode.cpptools
-code --install-extension Equinusocio.vsc-material-theme
-code --install-extension PKief.material-icon-theme
-code --install-extension bungcip.better-toml
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension PeterJausovec.vscode-docker
 code --install-extension foxundermoon.shell-format
 code --install-extension truman.autocomplate-shell
-code --install-extension formulahendry.code-runner
-code --install-extension PeterJausovec.vscode-docker
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension PKief.material-icon-theme
+
+# Rust
+code --install-extension rust-lang.rust
+code --install-extension bungcip.better-toml
+
+# Python
 code --install-extension ms-python.python
+
+# Java
+code --install-extension vscjava.vscode-java-pack
