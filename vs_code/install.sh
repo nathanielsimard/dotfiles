0c2ac555c992168f5721ce5ca3a98d0c4e1efca6
nathaniel <nathaniel.simard.42@gmail.com>
2019-01-11 22:24:22 -0500

    Update Vs Code config && plugins

--- vs_code/install.sh
+++ vs_code/install.sh
@@ -3,7 +3,7 @@
 echo "Installing extensions"
 # General
 code --install-extension vscodevim.vim
-code --install-extension hoovercj.vscode-settings-cycler
+code --install-extension eamodio.gitlens
 code --install-extension DavidAnson.vscode-markdownlint
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension Equinusocio.vsc-material-theme
@@ -13,11 +13,12 @@ code --install-extension PKief.material-icon-theme
 code --install-extension rust-lang.rust
 code --install-extension bungcip.better-toml
 
+# Go
+code --install-extension ms-vscode.Go
+
 # Python
 code --install-extension ms-python.python
-
-# Java
-code --install-extension vscjava.vscode-java-pack
+code --install-extension njpwerner.autodocstring
 
 echo "Installing Vs Code config"
 cp keybindings.json ~/.config/Code/User/
