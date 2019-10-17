414cb62e86abf5682ec680b31faa9e8c7a5c8bda
nathaniel <nathaniel.simard.42@gmail.com>
2019-10-16 22:43:33 -0400

    Update VsCode + Gnome Font Scaling

---
+++ gnome/scaling.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import subprocess
+
+
+def current_scaling_factor():
+    scaling = subprocess.check_output(
+        ["gsettings", "get", "org.gnome.desktop.interface", "text-scaling-factor"]
+    )
+    return float(scaling.decode("utf-8").replace("\n", ""))
+
+
+def set_scaling_factor(scaling=1.5):
+    subprocess.check_call(
+        [
+            "gsettings",
+            "set",
+            "org.gnome.desktop.interface",
+            "text-scaling-factor",
+            str(scaling),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    if current_scaling_factor() == 1.0:
+        set_scaling_factor()
+    else:
+        set_scaling_factor(1.0)
